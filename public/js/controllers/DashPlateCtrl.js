8799ae0fb38d9b994ce7c1176d25a76a597ec3ab
Ahsan Javaid <ahsan.javaid@novatoresols.com>
2015-10-09 21:15:59 +0500

    button and chart fixed on dashPlate

--- public/js/controllers/DashPlateCtrl.js
+++ public/js/controllers/DashPlateCtrl.js
@@ -1,14 +1,20 @@
-angular.module('DashPlateCtrl', []).controller('DashPlateController', function($scope) {
+angular.module('DashPlateCtrl', []).controller('DashPlateController', function($scope , $mdDialog ) {
 
+
+
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 	$(function() {
 		var chart = new Highcharts.Chart({
+			title:{
+				text:''
+			},
 			chart: {
 				renderTo: 'container',
 				type: 'column'
 			},
 			yAxis: {
 				title: {
-					text: 'axis title',
+					text: 'Price',
 					useHTML: true,
 					style: {
 						"-webkit-transform": "rotate(90deg)",
@@ -25,4 +31,66 @@ angular.module('DashPlateCtrl', []).controller('DashPlateController', function($
 		});
 	});
 
+	$scope.alert = '';
+	$scope.showAlert = function(ev) {
+		// Appending dialog to document.body to cover sidenav in docs app
+		// Modal dialogs should fully cover application
+		// to prevent interaction outside of dialog
+		$mdDialog.show(
+			$mdDialog.alert()
+				.parent(angular.element(document.body))
+				.title('This is an alert title')
+				.content('You can specify some description text in here.')
+				.ariaLabel('Alert Dialog Demo')
+				.ok('Got it!')
+				.targetEvent(ev)
+		);
+	};
+
+	//email address from first page is shown here
+	$scope.showConfirm = function(ev) {
+		var confirm = $mdDialog.confirm()
+			.title('Buy / Sell whatever they click on.')
+			.content('I want to buy this plate. Confirm your email address and submit ')
+			.ariaLabel('Lucky day')
+			.ok('Okay')
+			.cancel('Not')
+			.targetEvent(ev);
+		$mdDialog.show(confirm).then(function() {
+			$scope.alert = 'You decided to get rid of your debt.';
+		}, function() {
+			$scope.alert = 'You decided to keep your debt.';
+		});
+	};
+
+	$scope.openConfirmationDialog = function(ev, type){
+		$mdDialog.show({
+			controller: confirmationController,
+			templateUrl: '/views/buy-sell-dialog.html',
+			locals:{
+				"type": type
+			},
+			targetEvent: ev
+		})
+			.then(function(data) {
+
+			}, function() {
+			});
+	}
+
+
+
+
+
 });
+
+function confirmationController($scope, type, $mdDialog){
+
+
+	$scope.type = type;
+	console.log(type)
+	$scope.close = function() {
+		$mdDialog.hide();
+	};
+
+}
